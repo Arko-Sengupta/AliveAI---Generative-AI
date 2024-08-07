e2ff45fbe39c501121cc6819063d7eceab163c1d
Arko-Sengupta <arkosengupta700@gmail.com>
2024-08-07 13:40:05 +0530

    Rebase

--- Frontend/src/Components/Contact.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Contact = () => {
-    return (
-        <h1>This is Contact</h1>
-    );
-};
-
-export default Contact;
--- Frontend/src/Components/Feature.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Feature = () => {
-    return (
-        <h1>This is Feature</h1>
-    );
-};
-
-export default Feature;
