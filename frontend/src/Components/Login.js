364562fbe091ddfa4a202be089e556ba33a72854
mdanas00 <mohd44anas@gmail.com>
2024-08-13 23:14:00 +0530

    Error handling login and singnup | Fixed white space in UI | Code cleanuo

--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -1,16 +1,24 @@
 import React, { useState } from "react";
-import { Container, Row, Col, Form, Button, Alert, InputGroup } from "react-bootstrap";
+import {
+  Alert,
+  Button,
+  Col,
+  Container,
+  Form,
+  InputGroup,
+  Row,
+} from "react-bootstrap";
+import { FaEnvelope, FaEye, FaEyeSlash, FaLock } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
-import { useAuth } from "./Routes/AuthContext";
-import { FaEye, FaEyeSlash, FaEnvelope, FaLock } from "react-icons/fa";
 import Swal from "sweetalert2";
-
-import Logo from "../Utils/Images/Logo Images/AliveAI Logo.png";
 import "../StyleSheets/Dashboard.css";
+import Logo from "../Utils/Images/Logo Images/AliveAI Logo.png";
+import { useAuth } from "./Routes/AuthContext";
 
 // Password format validation function
 const handlePasswordFormat = (password) => {
-  const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[^A-Za-z0-9])(?!.*\s).{8,18}$/;
+  const passwordRegex =
+    /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[^A-Za-z0-9])(?!.*\s).{8,18}$/;
   return passwordRegex.test(password);
 };
 
@@ -49,7 +57,8 @@ const handleLogin = async (
       await sleep(2000);
 
       // Replace "web_token" with your actual JWT token
-      const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3R1c2VyIiwiZW1haWwiOiJ0ZXN0dXNlckBleGFtcGxlLmNvbSIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNjEwNzU4MjI2LCJleHAiOjE2MTA3NjE4MjZ9.02F2ZuK3GOl9BptxIjJh1AZLa_Eklj4P9hZjCv7uPVU';
+      const token =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3R1c2VyIiwiZW1haWwiOiJ0ZXN0dXNlckBleGFtcGxlLmNvbSIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNjEwNzU4MjI2LCJleHAiOjE2MTA3NjE4MjZ9.02F2ZuK3GOl9BptxIjJh1AZLa_Eklj4P9hZjCv7uPVU";
 
       // Use the login function from useAuth() to set the token
       login(token);
@@ -69,21 +78,6 @@ const sleep = (ms) => {
 };
 
 // UI Components
-const FormAlert = ({ type, message, show, setShow }) => {
-  if (show) {
-    return (
-      <Alert
-        variant={type === "Congratulations!" ? "success" : "danger"}
-        onClose={() => setShow(false)}
-        dismissible
-      >
-        <h5>{type}</h5>
-        <p>{message}</p>
-      </Alert>
-    );
-  }
-  return null;
-};
 
 const FormLogo = () => {
   return (
@@ -93,10 +87,19 @@ const FormLogo = () => {
   );
 };
 
-const FormConstraints = ({ controlId, type, placeholder, value, onChange, icon, isPassword, togglePassword }) => {
-  const iconStyle = { color: '#009ab8' };
+const FormConstraints = ({
+  controlId,
+  type,
+  placeholder,
+  value,
+  onChange,
+  icon,
+  isPassword,
+  togglePassword,
+}) => {
+  const iconStyle = { color: "#009ab8" };
   return (
-    <Form.Group controlId={controlId} className="my-4">
+    <Form.Group controlId={controlId} className="mt-4">
       <InputGroup>
         <InputGroup.Text style={iconStyle}>{icon}</InputGroup.Text>
         <Form.Control
@@ -106,8 +109,11 @@ const FormConstraints = ({ controlId, type, placeholder, value, onChange, icon, 
           onChange={onChange}
         />
         {isPassword && (
-          <InputGroup.Text onClick={togglePassword} style={{ ...iconStyle, cursor: "pointer" }}>
-            {type === "password" ? <FaEye style={iconStyle} /> : <FaEyeSlash style={iconStyle} />}
+          <InputGroup.Text
+            onClick={togglePassword}
+            style={{ cursor: "pointer" }}
+          >
+            {type === "password" ? <FaEye /> : <FaEyeSlash />}
           </InputGroup.Text>
         )}
       </InputGroup>
@@ -126,14 +132,18 @@ const LoginButton = ({ StaticData }) => {
 const LoginDashboardNavigate = ({ StaticData, navigate, onForgotPassword }) => {
   return (
     <>
-      <div className="text-center mt-4">
-        <span
-          className="forgot-password-text"
-          onClick={onForgotPassword}
-        >
-          {StaticData.Login.Login_Forgot_Password}
-        </span>
+      <div className="pt-1 d-flex justify-content-between align-items-center">
+        <div className="d-flex justify-content-between align-items-center">
+          <input type="checkbox" />
+          <label className="remember-me-text ms-1">Remember Me</label>
+        </div>
+        <div>
+          <span className="forgot-password-text" onClick={onForgotPassword}>
+            {StaticData.Login.Login_Forgot_Password}
+          </span>
+        </div>
       </div>
+      <LoginButton StaticData={StaticData} />
       <div className="mt-3 text-center">
         <p>
           {StaticData.Login.Login_Account_Exists}&nbsp;
@@ -163,95 +173,101 @@ const Login = ({ StaticData }) => {
       ? "Congratulations!"
       : "Authentication Failed";
 
-      const handleForgotPassword = async () => {
-        // Step 1: Show popup to enter new password and confirm password
-        const { value: formValues } = await Swal.fire({
-          title: 'Reset Password',
-          html: `
-            <input id="newPassword" class="swal2-input" placeholder="New Password" type="password">
-            <input id="confirmPassword" class="swal2-input" placeholder="Confirm Password" type="password">
-          `,
-          focusConfirm: false,
-          showCancelButton: true,
-          confirmButtonText: 'Submit',
-          cancelButtonText: 'Cancel',
-          preConfirm: () => {
-            const newPassword = Swal.getPopup().querySelector('#newPassword').value;
-            const confirmPassword = Swal.getPopup().querySelector('#confirmPassword').value;
-            if (newPassword !== confirmPassword) {
-              Swal.showValidationMessage('Passwords do not match');
-              return false;
-            }
-            if (!handlePasswordFormat(newPassword)) {
-              Swal.showValidationMessage('Password must be between 8 and 18 characters and include uppercase, lowercase, numeric, and special characters');
-              return false;
-            }
-            return { newPassword };
-          }
-        });
-    
-        if (formValues) {
-          // Step 2: Show OTP popup after closing the previous one
-          const { value: otp } = await Swal.fire({
-            title: 'Enter OTP',
-            input: 'number',
-            inputPlaceholder: 'Enter 6-digit OTP',
-            confirmButtonText: 'Verify',
-            showCancelButton: true,
-            cancelButtonText: 'Cancel',
-            preConfirm: (otp) => {
-              // Static OTP for validation
-              if (otp !== '123456') {
-                Swal.showValidationMessage('Incorrect OTP');
-                otp = "";
-                return false;
-              }
-              return true;
-            }
-          });
-    
-          if (otp) {
-            // Show success message and update the login password
-            Swal.fire({
-              title: 'Success',
-              text: 'Password has been updated successfully!',
-              icon: 'success',
-              confirmButtonText: 'OK'
-            }).then(() => {
-              // Proceed with login using the new password
-              handleLogin(
-                { preventDefault: () => {} },
-                navigate,
-                email,
-                setEmail,
-                formValues.newPassword,
-                setPassword,
-                setloginMessage,
-                setShow,
-                login
-              );
-            });
-          }
+  const handleForgotPassword = async () => {
+    const { value: otpValues } = await Swal.fire({
+      title: "Enter OTPs",
+      html: `
+        <input id="emailOtp" class="swal2-input" placeholder="Enter OTP sent to email" type="number" min='0'>
+        <input id="phoneOtp" class="swal2-input" placeholder="Enter OTP sent to phone" type="number" min='0'>
+      `,
+      focusConfirm: false,
+      showCancelButton: true,
+      confirmButtonText: "Verify",
+      cancelButtonText: "Cancel",
+      customClass: {
+        confirmButton: "btn-verify",
+      },
+      preConfirm: () => {
+        const emailOtp = Swal.getPopup().querySelector("#emailOtp").value;
+        const phoneOtp = Swal.getPopup().querySelector("#phoneOtp").value;
+
+        if (!/^\d{6}$/.test(emailOtp)) {
+          Swal.showValidationMessage("Email OTP must be exactly 6 digits");
+          return false;
         }
-      };
+        if (!/^\d{6}$/.test(phoneOtp)) {
+          Swal.showValidationMessage("Phone OTP must be exactly 6 digits");
+          return false;
+        }
+        if (emailOtp !== "123456") {
+          Swal.showValidationMessage("Incorrect OTP for email");
+          return false;
+        }
+        if (phoneOtp !== "654321") {
+          Swal.showValidationMessage("Incorrect OTP for phone");
+          return false;
+        }
+        return { emailOtp, phoneOtp };
+      },
+    });
+
+    if (otpValues) {
+      // Step 1: Show popup to enter new password and confirm password
+      const { value: formValues } = await Swal.fire({
+        title: "Reset Password",
+        html: `
+                  <input id="newPassword" class="swal2-input" placeholder="New Password" type="password">
+                  <input id="confirmPassword" class="swal2-input" placeholder="Confirm Password" type="password">
+                `,
+        focusConfirm: false,
+        showCancelButton: true,
+        confirmButtonText: "Submit",
+        cancelButtonText: "Cancel",
+        preConfirm: () => {
+          const newPassword =
+            Swal.getPopup().querySelector("#newPassword").value;
+          const confirmPassword =
+            Swal.getPopup().querySelector("#confirmPassword").value;
+          if (newPassword !== confirmPassword) {
+            Swal.showValidationMessage("Passwords do not match");
+            return false;
+          }
+          if (!handlePasswordFormat(newPassword)) {
+            Swal.showValidationMessage(
+              "Password must be between 8 and 18 characters and include uppercase, lowercase, numeric, and special characters"
+            );
+            return false;
+          }
+          return { newPassword };
+        },
+      });
+
+      if (formValues) {
+        Swal.fire({
+          title: "Success",
+          text: "Password has been updated successfully!",
+          icon: "success",
+          confirmButtonText: "OK",
+        });
+      }
+    }
+  };
+
+  show &&
+    Swal.fire({
+      icon: "error",
+      title: AlertType,
+      text: loginMessage,
+      allowOutsideClick: false,
+      allowEscapeKey: false,
+    }).then((res) => {
+      if (res.isConfirmed) {
+        setShow(false);
+      }
+    });
 
   return (
     <>
-      <Container
-        fluid
-        className="d-flex justify-content-center align-items-center max-vh-50 bg-dark text-light"
-      >
-        <Row className="form-alert w-100 mx-2">
-          {
-            <FormAlert
-              type={AlertType}
-              message={loginMessage}
-              show={show}
-              setShow={setShow}
-            />
-          }
-        </Row>
-      </Container>
       <Container
         fluid
         className="d-flex justify-content-center align-items-center min-vh-100 bg-dark text-light"
@@ -296,7 +312,6 @@ const Login = ({ StaticData }) => {
                   isPassword
                   togglePassword={() => setShowPassword(!showPassword)}
                 />
-                <LoginButton StaticData={StaticData} />
                 <LoginDashboardNavigate
                   StaticData={StaticData}
                   navigate={navigate}
